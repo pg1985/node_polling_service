d77198fcab52aca19a0b15fdf393dbc87eced9be
pg1985 <patrick@pushup.com>
2015-07-09 16:23:25 -0500

    fixed package.json and added skeleton for PollerManager

--- main.js
+++ main.js
@@ -1,13 +1,3 @@
-var express = require('express');
 var request = require('request');
 var url     = require('url');
 
-var app = express();
-
-app.get('/', function(req, res) {
-	res.json('{"this": true}');
-});
-
-app.listen('9917');
-
-exports = module.exports = app;
