a6b16cd068f5f9bb62d21a398b1bbba8fa4be33f
pg1985 <patrick@pushup.com>
2015-07-13 16:43:22 -0500

    Changed loading order, should not have async DB problems

--- main.js
+++ main.js
@@ -5,5 +5,5 @@ var PollerManager   = require('./lib/manager/PollerManager');
 var DatabaseManager = require('./lib/manager/DatabaseManager')
 var MQManager       = require('./lib/utils/Rabbitmq');
 
-var pollerManager = new PollerManager(); //automatically adding it's own data
 var databaseManager = new DatabaseManager();
+var pollerManager = new PollerManager(); 
