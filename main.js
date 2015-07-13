affb0a0a00ceead0f7744495c293c037ecdc9f13
pg1985 <patrick@pushup.com>
2015-07-13 11:29:37 -0500

    Added database/MQ empty classes, progress mainly on db functionality

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 var request = require('request');
 var url     = require('url');
 
-var PollerManager = require('./lib/manager/PollerManager')
+var PollerManager   = require('./lib/manager/PollerManager');
+var DatabaseManager = requre('./lib/utils/database');
+var MQManager       = require('./lib/utils/rabbitmq');
 
 var manager = new PollerManager(); //automatically adding it's own data
 
