a5afbcf764640a2b3a8b5d89a29c9d279b03ca39
pg1985 <patrick@pushup.com>
2015-07-14 16:17:45 -0500

    MongoDB post-checking functionality in place

--- main.js
+++ main.js
@@ -2,8 +2,6 @@ var request = require('request');
 var url     = require('url');
 
 var PollerManager   = require('./lib/manager/PollerManager');
-var DatabaseManager = require('./lib/manager/DatabaseManager')
 var MQManager       = require('./lib/utils/Rabbitmq');
 
-var databaseManager = new DatabaseManager();
 var pollerManager = new PollerManager(); 
