55b6b022a84958da1972d9de80fb7a20d3a717fc
pg1985 <patrick@pushup.com>
2015-07-15 11:36:11 -0500

    Removing worthless eventEmitter system, adding a mediator class

--- main.js
+++ main.js
@@ -4,5 +4,5 @@ var url     = require('url');
 var PollerManager   = require('./lib/manager/PollerManager');
 var MQManager       = require('./lib/utils/Rabbitmq');
 
+var mqManager = new MQManager();
 var pollerManager = new PollerManager(); 
-var mqManager = new MQManager;
