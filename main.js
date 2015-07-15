34c347778fb8c70d8fab2c84a1d7fb0d5654372c
pg1985 <patrick@pushup.com>
2015-07-15 14:32:52 -0500

    Got MQ to finally work, stupid ) didn't throw an error

--- main.js
+++ main.js
@@ -5,4 +5,4 @@ var PollerManager   = require('./lib/manager/PollerManager');
 var MQManager       = require('./lib/utils/Rabbitmq');
 
 var mqManager = new MQManager();
-var pollerManager = new PollerManager(); 
+var pollerManager = new PollerManager(mqManager); 
